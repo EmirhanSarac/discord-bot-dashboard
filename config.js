826ebe8739a20b2620cc0e54a5f74657100e8eb4
toprakozdemir <81726868+toprakozdemir@users.noreply.github.com>
2021-04-05 09:29:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     "ownerID": "282453998529806338", //kendi IDınızı yazınız
     "admins": [""],
     "support": [""],
-    "token": "", //botunuzun tokenini yazınız
+    "token": "ODI4NTE2MjM4MjQ2NDEyMzA4.YGqt4A.zcdIpjNoCVcus15g94xku_j72uc", //botunuzun tokenini yazınız
     "dashboard" : {
       "oauthSecret": "", //botunuzun secretini yazınız
       "callbackURL": `https://dash.vortexbot.org/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
